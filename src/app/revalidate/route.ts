5e1730db9c37f2ca520c6a1a29715fe85fb22b96
goncy <gonzalo.pozzo4@gmail.com>
2024-01-16 10:28:34 -0300

    Revlidate path

--- src/app/revalidate/route.ts
+++ src/app/revalidate/route.ts
@@ -1,6 +1,6 @@
 import type {NextRequest} from "next/server";
 
-import {revalidateTag} from "next/cache";
+import {revalidatePath} from "next/cache";
 
 export async function POST(request: NextRequest) {
   const params = request.nextUrl.searchParams;
@@ -12,7 +12,7 @@ export async function POST(request: NextRequest) {
     });
   }
 
-  revalidateTag(params.get("tag")!);
+  revalidatePath("/");
 
   return Response.json({revalidated: true});
 }
