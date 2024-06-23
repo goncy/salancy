575e14d67cfcd61f7790ff66a4a13af22975a822
goncy <gonzalo.pozzo4@gmail.com>
2024-06-23 15:33:55 -0300

    Parallelize calls

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,13 +5,18 @@ import HomePageClient from "./page.client";
 // Force static generation
 export const dynamic = "force-static";
 
-// Revalidate every month
-export const revalidate = 60 * 60 * 24 * 7 * 4;
+// Revalidate every week
+export const revalidate = 604800;
 
 export default async function Home() {
-  const salaries = await api.salary.list();
-  const dollarPrice = await api.dollar.price();
-  const inflation = await api.inflation.fetch();
+  // Fetch all data in parallel
+  const [salaries, dollarPrice, inflation] = await Promise.all([
+    api.salary.list(),
+    api.dollar.price(),
+    api.inflation.fetch(),
+  ]);
+
+  // This one not just depends on salaries but it's not an actual external call
   const options = await api.options.list(salaries);
 
   return (
